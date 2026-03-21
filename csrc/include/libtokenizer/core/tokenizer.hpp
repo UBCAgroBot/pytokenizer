408365332eb268708add10d3c63c0b835dbe6025
Markus Chu <markuschu06@gmail.com>
2026-03-21 12:48:31 -0700

    Fix build errors: remove non-existent benchmark dir and add missing method declarations

--- csrc/include/libtokenizer/core/tokenizer.hpp
+++ csrc/include/libtokenizer/core/tokenizer.hpp
@@ -10,6 +10,8 @@ namespace core {
 
 class Tokenizer {
 public:
+    static std::map<std::pair<int, int>, int> get_stats(const std::vector<int>& ids);
+    static std::vector<int> merge(const std::vector<int>& ids, const std::pair<int, int>& pair, int idx);
     std::string foo();
 };
 
