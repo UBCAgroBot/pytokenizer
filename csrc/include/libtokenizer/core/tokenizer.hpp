08d2dac022d8bf14ab156809f5d0fb37e7e0dbad
Markus8888888 <buspoko@gmail.com>
2026-03-21 12:53:47 -0700

    Revert "Markus c++"

--- csrc/include/libtokenizer/core/tokenizer.hpp
+++ csrc/include/libtokenizer/core/tokenizer.hpp
@@ -1,17 +1,13 @@
 #pragma once
 
-#include <vector>
-#include <map>
-#include <utility>
+#include <string>
 
 namespace pytokenizer {
 namespace core {
 
 class Tokenizer {
 public:
-    static std::map<std::pair<int, int>, int> get_stats(const std::vector<int>& ids);
-    static std::vector<int> merge(const std::vector<int>& ids, const std::pair<int, int>& pair, int idx);
-    void foo();
+    std::string foo();
 };
 
 } // namespace core
--- csrc/src/core/tokenizer.cpp
+++ csrc/src/core/tokenizer.cpp
@@ -1,36 +1,10 @@
-#include <algorithm>
+#include "libtokenizer/core/tokenizer.hpp"
 #include <iostream>
-#include <string>
-#include <vector>
-#include <map>
-#include "libtokenizer/libtokenizer.hpp"
 
 namespace pytokenizer {
 namespace core {
 
-std::map<std::pair<int, int>, int> Tokenizer::get_stats(const std::vector<int>& ids) {
-    std::map<std::pair<int, int>, int> counts;
-    for (size_t i = 0; i < ids.size() - 1; ++i) {
-        std::pair<int, int> pair = {ids[i], ids[i + 1]};
-        counts[pair]++;
-    }
-    return counts;
-}
-
-std::vector<int> Tokenizer::merge(const std::vector<int>& ids, const std::pair<int, int>& pair, int idx) {
-    std::vector<int> newids;
-    size_t i = 0;
-    while (i < ids.size()) {
-        if (i < ids.size() - 1 && ids[i] == pair.first && ids[i + 1] == pair.second) {
-            newids.push_back(idx);
-            i += 2;
-        } else {
-            newids.push_back(ids[i]);
-            i += 1;
-        }
-    }
-    return newids;
-}
+std::string Tokenizer::foo() { return "foo"; }
 
 } // namespace core
 } // namespace pytokenizer
