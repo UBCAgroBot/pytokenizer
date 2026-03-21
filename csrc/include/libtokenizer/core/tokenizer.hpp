d8b6024518f75ee617d0f18e466be19e4d226cc4
Markus Chu <markuschu06@gmail.com>
2026-03-21 12:28:29 -0700

    Python implementation of BPE

--- csrc/include/libtokenizer/core/tokenizer.hpp
+++ csrc/include/libtokenizer/core/tokenizer.hpp
@@ -1,10 +1,16 @@
 #pragma once
 
+#include <vector>
+#include <map>
+#include <utility>
+
 namespace pytokenizer {
 namespace core {
 
 class Tokenizer {
 public:
+    static std::map<std::pair<int, int>, int> get_stats(const std::vector<int>& ids);
+    static std::vector<int> merge(const std::vector<int>& ids, const std::pair<int, int>& pair, int idx);
     void foo();
 };
 
--- csrc/src/core/tokenizer.cpp
+++ csrc/src/core/tokenizer.cpp
@@ -1,10 +1,36 @@
-#include "libtokenizer/core/tokenizer.hpp"
+#include <algorithm>
 #include <iostream>
+#include <string>
+#include <vector>
+#include <map>
+#include "libtokenizer/libtokenizer.hpp"
 
 namespace pytokenizer {
 namespace core {
 
-void Tokenizer::foo() { std::cout << "foo" << std::endl; }
+std::map<std::pair<int, int>, int> Tokenizer::get_stats(const std::vector<int>& ids) {
+    std::map<std::pair<int, int>, int> counts;
+    for (size_t i = 0; i < ids.size() - 1; ++i) {
+        std::pair<int, int> pair = {ids[i], ids[i + 1]};
+        counts[pair]++;
+    }
+    return counts;
+}
+
+std::vector<int> Tokenizer::merge(const std::vector<int>& ids, const std::pair<int, int>& pair, int idx) {
+    std::vector<int> newids;
+    size_t i = 0;
+    while (i < ids.size()) {
+        if (i < ids.size() - 1 && ids[i] == pair.first && ids[i + 1] == pair.second) {
+            newids.push_back(idx);
+            i += 2;
+        } else {
+            newids.push_back(ids[i]);
+            i += 1;
+        }
+    }
+    return newids;
+}
 
 } // namespace core
 } // namespace pytokenizer
